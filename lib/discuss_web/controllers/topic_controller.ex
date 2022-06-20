a5878fa79d12a0360400482ce481c79ffb26256e
Alex Gochenour <alexgochenour@gmail.com>
2022-06-20 17:23:51 -0400

    feat: Added an index page

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -1,12 +1,15 @@
 defmodule DiscussWeb.TopicController do
   use DiscussWeb, :controller
   alias Discuss.Repo
-
   alias DiscussWeb.Topic
+
+  def index(conn, _params) do
+    topics = Repo.all(Topic) #in iex, don't forget the `Web`:  Discuss.Repo.all(DiscussWeb.Topic)
+    render conn, "index.html.eex", topics: topics
+  end
 
   def new(conn, _params) do
     changeset = Topic.changeset(%Topic{}, %{})
-
     render conn, "new.html", changeset: changeset
   end
 
