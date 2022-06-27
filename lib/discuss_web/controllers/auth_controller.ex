02ff2d29dfa74bdf499719f14731c0e80f01beb8
Alex Gochenour <alexgochenour@gmail.com>
2022-06-26 23:21:35 -0400

    feat: Auth controller now checks for the user in the database before deciding whether to add them.

--- lib/discuss_web/controllers/auth_controller.ex
+++ lib/discuss_web/controllers/auth_controller.ex
@@ -1,13 +1,21 @@
 defmodule DiscussWeb.AuthController do
   use DiscussWeb, :controller
   plug Ueberauth
+  alias DiscussWeb.User
 
-  def callback(conn, params) do
-    IO.puts "++++++++++"
-    IO.inspect(conn.assigns)
-    IO.puts "++++++++++"
-    IO.inspect(params)
-    IO.puts "++++++++++"
+  def callback(%{assigns: %{ueberauth_auth: auth}} = conn, params) do
+    user_params = %{token: auth.credentials.token, email: auth.info.email, provider: "github"}
+    changeset = User.changeset(%User{}, user_params)
+
+    insert_or_update_user(changeset)
   end
 
+  defp insert_or_update_user(changeset) do
+    case Repo.get_by(User, email: changeset.changes.email) do
+      nil ->
+        Repo.insert(changeset)
+      user ->
+        {:ok, user}
+    end
+  end
 end
